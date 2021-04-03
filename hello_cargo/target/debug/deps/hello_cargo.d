05ddc8f9cb024a46335fb7f091abca1e04fe953c
Angela Lu <angxlfish@gmail.com>
2021-04-03 13:29:33 -0700

    hcargos a homie

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/alu/Desktop/notcRUSTy/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/alu/Desktop/notcRUSTy/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/alu/Desktop/notcRUSTy/hello_cargo/target/debug/hello_cargo: /Users/alu/Desktop/notcRUSTy/hello_cargo/src/main.rs
